8bf709d1f020fdd753e598a32f6dbd8e0e96b42a
Alexander Yablokov <tajikistan@rambler.ru>
2023-01-08 10:39:48 +0500

    Working verson without input validation. Single file. Single module.

---
+++ bmi.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"runtime"
+	"strconv"
+	"strings"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+
+func main() {
+	// Output information
+	fmt.Println("BMI Calculator")
+	fmt.Println("------------------")
+	// Prompt for user input (weight and height)
+	fmt.Print("Please enter your weight (kg): ")
+	weightInput, _ := reader.ReadString('\n')
+
+	fmt.Print("Please enter your height (m): ")
+	heightInput, _ := reader.ReadString('\n')
+
+	// Save that user input in variables
+	if runtime.GOOS == "windows" {
+		weightInput = strings.Replace(weightInput, "\r\n", "", -1)
+		heightInput = strings.Replace(heightInput, "\r\n", "", -1)
+	} else {
+		weightInput = strings.Replace(weightInput, "\n", "", -1)
+		heightInput = strings.Replace(heightInput, "\n", "", -1)
+	}
+
+	weight, _ := strconv.ParseFloat(weightInput, 64)
+	height, _ := strconv.ParseFloat(heightInput, 64)
+
+	// Calculate the BMI (weight / (height * height) )
+	bmi := weight / (height * height)
+
+	// Output the calculate BMI
+	fmt.Printf("Your BMI: %.2f", bmi)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/alexander-yablokov/go-bmi
+
+go 1.18
